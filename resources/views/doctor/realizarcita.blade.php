8c8e3a3ddaa2209615ed344ddd6d69b59b367834
Isai1024 <2130180@upv.edu.mx>
2024-07-08 13:19:40 -0600

    mas

--- resources/views/doctor/realizarcita.blade.php
+++ resources/views/doctor/realizarcita.blade.php
@@ -9,52 +9,84 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 shadow-md sm:rounded-lg p-6">
                 <div class="p-6 rounded-lg shadow-lg">
-                    <div class="grid grid-cols-2 gap-4 mb-4">
-                        <div>
-                            <label for="nombre" class="block text-sm font-medium text-white">Nombre</label>
-                            <label type="text" id="nombre"
-                                class="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500">
-                                {{ $consulta->paciente->nombre }} {{ $consulta->paciente->apellido_p }} {{ $consulta->paciente->apellido_m }}
-                            </label>
-                        </div>
-                        <div>
-                            <label for="fecha-hora" class="block text-sm font-medium text-white">Fecha y hora</label>
-                            <label type="text" id="fecha-hora"
-                                class="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500">
+                    <form action="">
+                        <div class="grid grid-cols-2 gap-4 mb-4">
+                            <div>
+                                <label for="nombre" class="block text-sm font-medium text-white">Nombre</label>
+                                <label type="text" id="nombre"
+                                    class="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500">
+                                    {{ $consulta->paciente->nombre }} {{ $consulta->paciente->apellido_p }}
+                                    {{ $consulta->paciente->apellido_m }}
+                                </label>
+                                <input type="hidden" id="paciente_id" name="paciente_id" value="{{ $consulta->paciente_id }}">
+                            </div>
+                            <div>
+                                <label for="fecha-hora" class="block text-sm font-medium text-white">Fecha y
+                                    hora</label>
+                                <label type="text" id="fecha-hora"
+                                    class="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500">
                                     {{ $consulta->fecha }} | {{ $consulta->hora }}
-                            </label>
+                                </label>
+                                <input type="hidden" id="fecha" name="fecha" value="{{ $consulta->fecha }}">
+                            </div>
                         </div>
-                    </div>
-                    <div class="grid grid-cols-2 gap-4 mb-4">
-                        <div>
-                            <label for="cuenta-pagada" class="block text-sm font-medium text-white">
-                                Cuenta pagada
-                            </label>
-                            <label type="text" id="cuenta-pagada"
+                        <div class="grid grid-cols-2 gap-4 mb-4">
+                            <div>
+                                <label for="cuenta-pagada" class="block text-sm font-medium text-white">
+                                    Cuenta pagada
+                                </label>
+                                <label type="text" id="cuenta-pagada"
+                                    class="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500">
+                                    {{ $consulta->pagada ? 'Si' : 'No' }}
+                                </label>
+                            </div>
+                            <div>
+                                <label for="tratamiento"
+                                    class="block text-sm font-medium text-white">Tratamiento</label>
+                                <a href="{{ route('doctor.servicios', $consulta->id) }}" id="btn-tratamiento"
+                                    class="text-center block w-full mt-1 py-2.5 font-medium text-white bg-indigo-600 rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                                    Agregar tratamiento
+                                </a>
+                            </div>
+                        </div>
+                        <div class="mb-4">
+                            <label for="motivos" class="block text-sm font-medium text-white">Motivos</label>
+                            <input type="text" id="motivos" value="{{ $consulta->motivo }}"
                                 class="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500">
-                                {{ $consulta->pagada ? 'Si' : 'No' }}
-                            </label>
                         </div>
+
+                        <!-- Consulta -->
+
+                        <div class="mb-4">
+                            <label for="talla" class="block text-sm font-medium text-white">Talla:</label>
+                            <input type="number" id="talla" name="talla"
+                                class="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500">
+                        </div>
+
+                        <div class="mb-4">
+                            <label for="peso" class="block text-sm font-medium text-white">Peso:</label>
+                            <input type="number" id="peso" name="peso"
+                                class="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500">
+                        </div>
+
+                        <div class="mb-4">
+                            <label for="temperatura" class="block text-sm font-medium text-white">Temperatura en grados C°:</label>
+                            <input type="number" id="temperatura" name="temperatura"
+                                class="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500">
+                        </div>
+
+                        <div class="mb-4">
+                            <label for="presion" class="block text-sm font-medium text-white">Presion:</label>
+                            <input type="number" id="presion" name="presion"
+                                class="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500">
+                        </div>
+                        
                         <div>
-                            <label for="tratamiento" class="block text-sm font-medium text-white">Tratamiento</label>
-                            <a
-                                href="{{ route('doctor.servicios', $consulta->id) }}"
-                                id="btn-tratamiento"
-                                class="text-center block w-full mt-1 py-2.5 font-medium text-white bg-indigo-600 rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                                Agregar tratamiento
-                            </a>
+                            <label for="notas" class="block text-sm font-medium text-white">Notas de cita</label>
+                            <textarea id="notas" name="notas" rows="6"
+                                class="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500">{{ $consulta->observaciones ? $consulta->observaciones : '' }}</textarea>
                         </div>
-                    </div>
-                    <div class="mb-4">
-                        <label for="motivos" class="block text-sm font-medium text-white">Motivos</label>
-                        <input type="text" id="motivos" value="{{ $consulta->motivo }}"
-                            class="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500">
-                    </div>
-                    <div>
-                        <label for="notas" class="block text-sm font-medium text-white">Notas de cita</label>
-                        <textarea id="notas" rows="6"
-                            class="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500">{{ ($consulta->observaciones) ? $consulta->observaciones : "" }}</textarea>
-                    </div>
+                    </form>
                     <br>
                     @if ($consulta->pagada)
                         <div>
